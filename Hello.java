55524a0ff0999251328f908b7838a08f051bcac3
mukulsustcse <mukul.cse2010@gmail.com>
2020-07-25 19:29:04 +0600

    Added a new method called greeting

--- Hello.java
+++ Hello.java
@@ -1,5 +1,11 @@
 public class Hello {
     public static void main(String[] args) {
         System.out.println("Git is awesome");
+
+        greeting("MUKUL");
+    }
+
+    public static void greeting(String name) {
+        System.out.println("Welcome "+ name);
     }
 }
