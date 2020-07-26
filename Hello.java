0cde0e5e83f564335425ca0c15810e9969204a12
mukulsustcse <mukul.cse2010@gmail.com>
2020-07-26 12:53:48 +0600

    all method removed and add message method

--- Hello.java
+++ Hello.java
@@ -3,21 +3,10 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("Learning Git Merge and Merge conflict...");
 
-        display();
-        
-        System.out.println("CUBE value of 4 is: "+ sqr(4));
+        message();
     }
 
-    
-    public static void greeting(String name) {
-        System.out.println("Bye bye "+ name);
-    }
-    
-        public static void display() {
-        System.out.println("Hey hey hey");
-    }
-
-    public static int sqr(int x) {
-        return x*x*x;
+        public static void message() {
+        System.out.println("Yahoo! now are able to contribute on open source...");
     }
 }
