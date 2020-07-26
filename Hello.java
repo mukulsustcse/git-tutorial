813c46863a5770a9d588483e74e7131def59ab13
mukulsustcse <mukul.cse2010@gmail.com>
2020-07-26 12:31:21 +0600

    remove greeting and add display to branch A

--- Hello.java
+++ Hello.java
@@ -1,18 +1,18 @@
 
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("Git is awesome");
+        System.out.println("Learning Git Merge and Merge conflict...");
 
-        greeting("MUKUL");
+        display();
         
-        System.out.println("Square value of 4 is: "+ sqr(4));
+        System.out.println("CUBE value of 4 is: "+ sqr(4));
     }
 
-    public static void greeting(String name) {
-        System.out.println("Welcome "+ name);
+    public static void display() {
+        System.out.println("Hey hey hey");
     }
 
     public static int sqr(int x) {
-        return x*x;
+        return x*x*x;
     }
 }
