197df843969beb50b8793f4d8ed5b17228ec3266
mukulsustcse <mukul.cse2010@gmail.com>
2020-07-26 11:55:05 +0600

    commit again and again

--- Hello.java
+++ Hello.java
@@ -1,12 +1,18 @@
+
 public class Hello {
     public static void main(String[] args) {
         System.out.println("Git is awesome");
 
-        // greeting("MUKUL");
-        greeting("Git");
+        greeting("MUKUL");
+        int p = sqr(4);
+        System.out.println(p);
     }
 
     public static void greeting(String name) {
-        System.out.println("Bye Bye "+ name);
+        System.out.println("Welcome "+ name);
+    }
+
+    public static int sqr(int x) {
+        return x*x;
     }
 }
